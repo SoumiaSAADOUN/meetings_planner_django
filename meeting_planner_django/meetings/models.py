9ea923167a533eb6a583a89b3f25a8f25da953db
soumia_saadoun <es_saadoun@esi.dz>
2020-04-14 16:18:38 +0100

    room added

--- meeting_planner_django/meetings/models.py
+++ meeting_planner_django/meetings/models.py
@@ -2,9 +2,28 @@ from django.db import models
 from datetime import time
 
 # Create your models here.
+
+
+
+class Room (models.Model):
+    name = models.CharField(max_length=200, default=" ")
+    room_floor= models.IntegerField( default="1")
+    room_num =models.IntegerField(default="0")
+
+    def __str__(self):
+        return f"{self.name}: number {self.room_num} on the {self.room_floor} st/nd/th floor"
+
+
 class Meeting(models.Model):
     title = models.CharField(max_length=200)
     date =models.DateField()
     start_time=models.TimeField(default=time(9))
-    duration = models.IntegerField(default=9)
-    decription = models.TextField(max_length=1500, default=( 'Add description here ')
+    duration = models.IntegerField(default=2)
+    decription = models.TextField(max_length=1500, default=( 'Add description here '))
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return f"{self.title} at {self.start_time} on {self.date}"
+
+
